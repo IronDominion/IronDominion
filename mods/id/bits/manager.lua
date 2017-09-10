364bcb597b0166db21e5674874a630e3b8bad207
Jean-Rémy Buchs <jrb0001@692b8c32.de>
2017-09-10 02:04:29 +0200

    Fix and rework lua script

--- mods/id/bits/manager.lua
+++ mods/id/bits/manager.lua
@@ -1,71 +1,61 @@
-eraints = {3000, 4500, 4500, 4500, 4500, 4500, 4500, 4500, 4500, 4500} -- time untill another era in ticks 1500 = 1min
-eraunits = {"early_phase1", "early_phase2", "early_phase3", "mid_phase1", "mid_phase2", "mid_phase3", "late_phase1", "late_phase2", "late_phase3", "post_phase"} -- unit to spawn for prereq
-eraadvancetext= {
-  "You've advanced to Early Phase I.",
-  "You've advanced to Early Phase II.",
-  "You've advanced to Early Phase III.",
-  "You've advanced to Mid Phase I.",
-  "You've advanced to Mid Phase II.",
-  "You've advanced to Mid Phase III.",
-  "You've advanced to Late Phase I.",
-  "You've advanced to Late Phase II.",
-  "You've advanced to Late Phase III.",
-  "You've advanced to Post Phase X."} -- text to say when advancing
+eras = {
+  {["duration"] = 3000, ["prerequisite"] = "start_phase",  ["text"] = "The game has started."},
+  {["duration"] = 4500, ["prerequisite"] = "early_phase1", ["text"] = "You've advanced to Early Phase I."},
+  {["duration"] = 4500, ["prerequisite"] = "early_phase2", ["text"] = "You've advanced to Early Phase II."},
+  {["duration"] = 4500, ["prerequisite"] = "early_phase3", ["text"] = "You've advanced to Early Phase III."},
+  {["duration"] = 4500, ["prerequisite"] = "mid_phase1",   ["text"] = "You've advanced to Mid Phase I."},
+  {["duration"] = 4500, ["prerequisite"] = "mid_phase2",   ["text"] = "You've advanced to Mid Phase II."},
+  {["duration"] = 4500, ["prerequisite"] = "mid_phase3",   ["text"] = "You've advanced to Mid Phase III."},
+  {["duration"] = 4500, ["prerequisite"] = "late_phase1",  ["text"] = "You've advanced to Late Phase I."},
+  {["duration"] = 4500, ["prerequisite"] = "late_phase2",  ["text"] = "You've advanced to Late Phase II."},
+  {["duration"] = 4500, ["prerequisite"] = "late_phase3",  ["text"] = "You've advanced to Late Phase III."},
+  {["duration"] = 4500, ["prerequisite"] = "post_phase",   ["text"] = "You've advanced to Post Phase."}
+};
 
-erabools = {false, false, false, false, false, false, false, false, false, false} -- make one for each era
+currentEra = 1;
+remainingTime = eras[1].duration;
+players = nil;
 
-era_int_current = 1
-era_int_max = 0
-players = {}
-counter = 0
+if pcall(function() UserInterface.SetMissionText("") end) then
+  -- Not shellmap
+  WorldLoaded = function()
+    players = Player.GetPlayers(nil);
+  end
 
-WorldLoaded = function()
-  players = Player.GetPlayers(nil)
-  era_int_max = tablelength(eraunits) + 1 --because lua tables start from 1
-end
-
-Tick = function() 
-  if era_int_current >= era_int_max then
-    UserInterface.SetMissionText("")
-    return
+  Tick = function() 
+    if currentEra >= #eras then
+      UserInterface.SetMissionText("");
+      return;
     end
-  if counter >= eraints[era_int_current] and erabools[era_int_current] == false then
-    SpawnUnitForEachPlayer(eraunits[era_int_current]) 
-    Media.DisplayMessage(eraadvancetext[era_int_current])
-    erabools[era_int_current] = true
-    era_int_current = era_int_current + 1
-    counter = 0
-      if era_int_current >= era_int_max then
-    UserInterface.SetMissionText("")
-    return
+
+    if remainingTime < 0 then
+      currentEra = currentEra + 1;
+      remainingTime = eras[currentEra].duration;
+
+      SpawnUnitForEachPlayer(eras[currentEra].prerequisite);
+      Media.DisplayMessage(eras[currentEra].text);
+    end
+
+    remainingTime = remainingTime - 1;
+    UserInterface.SetMissionText("Next era in " .. SecondsToClock(remainingTime / DateTime.Seconds(1)) .. ".");
+  end
+
+  SpawnUnitForEachPlayer = function(ActorName)
+    for index, value in ipairs(players) do
+      Actor.Create(ActorName, true, {Owner = value, Location = CPos.New(0,0)});
     end
   end
-  counter = counter + 1
-  timeinclock = SecondsToClock((eraints[era_int_current] - counter) / 25)
-  UserInterface.SetMissionText("Next era in "..timeinclock..".")
-end
 
-SpawnUnitForEachPlayer = function(ActorName)
-    for index,value in ipairs(players) do 
-    local actor = Actor.Create(ActorName, true, {Owner = value, Location = CPos.New(0,0)})
+  function SecondsToClock(seconds)
+    local seconds = tonumber(seconds);
+
+    if seconds <= 0 then
+      return "00:00";
+    else
+      hours = string.format("%02.f", math.floor(seconds / 3600));
+      mins = string.format("%02.f", math.floor(seconds / 60 - (hours * 60)));
+      secs = string.format("%02.f", math.floor(seconds - hours * 3600 - mins * 60));
+      return mins .. ":" .. secs;
     end
-end
-
-function SecondsToClock(seconds)
-  local seconds = tonumber(seconds)
-
-  if seconds <= 0 then
-    return "00:00";
-  else
-    hours = string.format("%02.f", math.floor(seconds/3600));
-    mins = string.format("%02.f", math.floor(seconds/60 - (hours*60)));
-    secs = string.format("%02.f", math.floor(seconds - hours*3600 - mins *60));
-    return mins..":"..secs
   end
-end
-
-function tablelength(T)
-  local count = 0
-  for _ in pairs(T) do count = count + 1 end
-  return count
 end
